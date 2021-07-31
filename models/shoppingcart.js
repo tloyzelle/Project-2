e9ccac14aa016764801e74004fe3899659879a95
Cameron <cameron.blake465@gmail.com>
2021-07-31 14:01:20 -0400

    adding a wishlist and shopping cart model

---
+++ models/shoppingcart.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class shoppingCart extends Model {}
+
+shoppingCart.init(
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        primaryKey: true,
+        autoIncrement: true,
+      },
+      title: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      description: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      price: {
+          type: DataTypes.INTEGER,
+          allowNull: false
+      },
+      catItem_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'cat_item',
+          key: 'id',
+        },
+      },
+      dogItem_id: {
+        type: DataTypes.INTEGER,
+        references: {
+          model: 'dog_item',
+          key: 'id',
+        },
+      },
+    },
+    {
+      sequelize,
+      freezeTableName: true,
+      underscored: true,
+      modelName: 'shoppingCart',
+    }
+  );
+  
+  module.exports = shoppingCart;
+  
