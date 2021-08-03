65383a6a1f1a436813deabe6bdb8203e27ad7434
Brian Robinson <brob92993@gmail.com>
2021-08-03 19:22:52 -0400

    changed name of app

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -2,7 +2,7 @@ const router = require('express').Router();
 const { User } = require('../../models');
 const withAuth = require('../../utils/auth');
 
-router.post('/', withAuth, async (req, res) => {
+router.post('/', async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
