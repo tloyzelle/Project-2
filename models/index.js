ab630943046bdb215b445e0f0b34011ade553dbd
Brian Robinson <brob92993@gmail.com>
2021-08-03 17:58:41 -0400

    required the other models inside the model index.js

--- models/index.js
+++ models/index.js
@@ -1,3 +1,9 @@
 const User = require('./User');
+const dog_item = require('./dog_item');
+const cat_item = require ('./cat_item');
+const shoppingcart = require('./shoppingcart');
+const wishlist = require('./wishlist');
 
-module.exports = { User };
+
+
+module.exports = { User, dog_item, cat_item, shoppingcart, wishlist };
