f98077f9d9b8324b263c464753b03d5a91a06b42
Brian Robinson <brob92993@gmail.com>
2021-08-07 13:55:59 -0400

    updates to add to cart function

--- public/js/addtocart.js
+++ public/js/addtocart.js
@@ -1,8 +1,17 @@
 const addToCartFormHandler = async (event) => {
     event.preventDefault();
 
-
-
+ const response = await fetch('/api/cart', {
+        method: 'POST',
+        body: '',
+        headers: { 'Content-Type': 'application/json' }
+    });
+     if (response.ok) {
+      console.log(response);
+    } else {
+      alert('Failed to add to cart');
+    }
+  };
 
 
 
